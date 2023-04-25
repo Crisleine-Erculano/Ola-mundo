fe3fd0a06b4ff2db064f9c17bf75b6148cefc390
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-04-25 10:25:27 -0300

    Criando Componentes do react-router-dom.

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,9 +1,19 @@
+import Inicio from "./Paginas/inicio";
+import SobreMim from "./Paginas/inicio/SobreMim";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-      
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>} />
+        <Route path="/SobreMim" element={<SobreMim/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+
+
+
 }
 
 export default App;
